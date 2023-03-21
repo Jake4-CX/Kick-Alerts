8ded683964471e447978e64b458ce92856db6f48
= <jake44264@hotmail.co.uk>
2023-03-21 23:33:06 +0000

    Updated axios defaults & urls

--- src/API/Services/Emotes.ts
+++ src/API/Services/Emotes.ts
@@ -3,11 +3,11 @@ import axios from "../axios";
 export const Emotes = () => {
 
   const getStreamerEmotes = (streamer: string) => {
-    return axios.get(`/emotes/${streamer}`);
+    return axios.get(`https://kick.com/emotes/${streamer}`);
   };
 
   const getLocalEmotes = () => {
-    return axios.get(`https://localhost:5173/emotes_db.json`);
+    return axios.get(`/emotes_db.json`);
   };
   
 
--- src/API/axios.ts
+++ src/API/axios.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosError } from "axios";
 
-axios.defaults.baseURL = "https://kick.com/";
+//axios.defaults.baseURL = "https://kick.com/";
 
 export default axios;
